479f996ce18d0aea712b8e68a890e8aa0deb64d7
Matthew Muiter <muiterms@gmail.com>
2016-11-01 21:44:44 -0700

    changed files from tabs to spaces for indentation

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -6,46 +6,46 @@ const { bool, func, string } = PropTypes
 
 export default class Modal extends Component {
 
-	componentWillReceiveProps(nextProps) {
-		if (nextProps.show) {
-			document.getElementById(this.props.rootPageID).tabIndex = -1
-		} else {
-			document.getElementById(this.props.rootPageID).tabIndex = 0
-		}
-	}
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.show) {
+      document.getElementById(this.props.rootPageID).tabIndex = -1
+    } else {
+      document.getElementById(this.props.rootPageID).tabIndex = 0
+    }
+  }
 
-	render() {
-		if (this.props.show) {
-			return (
-				<div className="modal-container">
-					<div className="modal-title-container">
-						<span>{this.props.title}</span>
-						<span className="close-modal-container">
-							<span
-								aria-label="Click to close modal"
-								tabIndex="0"
-								onKeyDown={this.props.handleOnKeyDown}
-								onClick={this.props.handleOnClick}
-								className="close-modal">
-									&#215;
-							</span>
-						</span>
-					</div>
-					<div className="modal-content-container">
-					 {this.props.children}
-					</div>
-				</div>
-			)
-		} else {
-			return <noscript/>
-		}
-	}
+  render() {
+    if (this.props.show) {
+      return (
+        <div className="modal-container">
+          <div className="modal-title-container">
+            <span>{this.props.title}</span>
+            <span className="close-modal-container">
+              <span
+                aria-label="Click to close modal"
+                tabIndex="0"
+                onKeyDown={this.props.handleOnKeyDown}
+                onClick={this.props.handleOnClick}
+                className="close-modal">
+                  &#215;
+              </span>
+            </span>
+          </div>
+          <div className="modal-content-container">
+           {this.props.children}
+          </div>
+        </div>
+      )
+    } else {
+      return <noscript/>
+    }
+  }
 }
 
 Modal.propTypes = {
-	title: string,
-	rootPageID: string.isRequired,
-	show: bool.isRequired,
-	handleOnKeyDown: func,
-	handleOnClick: func
+  title: string,
+  rootPageID: string.isRequired,
+  show: bool.isRequired,
+  handleOnKeyDown: func,
+  handleOnClick: func
 }
